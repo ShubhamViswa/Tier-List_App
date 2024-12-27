3dae424037ef7de5c2fd0c38ca092041c6612c1f
Shubham Vishwakarma <shubhamviswa2000@gmail.com>
2024-12-27 20:38:46 +0530

    Added Some CSS and also functionalities

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+
+let currentDraggedItem;
+
 const tierInput = document.getElementById("tier");
 const submitBtn = document.getElementById("submit");
-const itemUrl = document.getElementById("tier-image");
+const itemUrl = document.getElementById("image-item");
 const submitBtn1 = document.getElementById("submit1")
+// const itemContainers = document.getElementsByClassName("item-container");
+
+// const tierLists = document.querySelectorAll(".tier-list");
 
 submitBtn.addEventListener("click", (event) => {
-    console.log("button is clicked")
+    // console.log("button is clicked")
     event.preventDefault();
     // const target = event.target;
     createTierList(tierInput.value);
@@ -13,11 +19,16 @@ submitBtn.addEventListener("click", (event) => {
 
 submitBtn1.addEventListener("click", (event) => {
     event.preventDefault();
+    if(itemUrl.value === ""){
+        alert("Please Enter a valid image URL");
+        return;
+    }
     createURLimage(itemUrl.value);
     itemUrl.value = "";
 })
 
 function createTierList(tierlistName){
+    
     const newTierList = document.createElement("div");
     newTierList.classList.add("tier-list");
 
@@ -26,18 +37,24 @@ function createTierList(tierlistName){
 
     const newtierlistItem = document.createElement("div");
     newtierlistItem.classList.add("tier-list-item");
-    // newtierlistItem.textContent = 
 
     newTierList.appendChild(heading);
     newTierList.appendChild(newtierlistItem);
 
     const tierSection = document.getElementById("tier-list-section");
     tierSection.appendChild(newTierList);
+
+    setUpDropZoneInTierList(newtierlistItem);
 }
 
 function createURLimage(itemURL){
     const newItem = document.createElement("div");
+    // newItem.setAttribute("class", "item-container");
+
+    setUpItemContainerForDrag(newItem);
+
     newItem.classList.add("item-container");
+    newItem.setAttribute("draggable", "true");
 
     const imageItem = document.createElement("img");
     imageItem.src = itemURL;
@@ -47,4 +64,28 @@ function createURLimage(itemURL){
     const nonTierSection = document.getElementById("non-tier-section");
 
     nonTierSection.appendChild(newItem);
+}
+
+const itemContainers = document.getElementsByClassName("item-container");
+
+for(const item of itemContainers){
+    setUpItemContainerForDrag(item);
+}
+
+function setUpItemContainerForDrag(item){
+    item.addEventListener("dragstart", (event) => {
+        // console.log(event.target.parentNode);
+        currentDraggedItem = event.target.parentNode;
+    })
+}
+
+function setUpDropZoneInTierList(tierList){
+     tierList.addEventListener("drop", (event) => {
+        event.preventDefault();
+     })
+
+     tierList.addEventListener("dragover", (event)=>{
+        console.log("dragged over a drop zone");
+        event.target.appendChild(currentDraggedItem);
+     })
 }
